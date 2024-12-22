9f40897e5b997e6bd80e821b50a6900db6b76a0c
Nemo_Crab <tarazevich.vladislav@gmail.com>
2024-12-22 04:46:06 +0300

    add Review work on frontend with backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRoute
 
 from app.review.router import router as router_review
@@ -8,5 +9,17 @@ from app.review.router import router as router_review
 app = FastAPI(
     title="CV frontend project"
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router_review)
